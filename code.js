3f830df962474db1d74283807c16818fca776b6b
manyaa20 <107490459+manyaa20@users.noreply.github.com>
2022-10-18 22:28:04 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+  
+// fs.mkdirSync("Document");
+
+// fs.writeFileSync("info.txt","My name is Manya Tyagi");
+
+// fs.appendFileSync('info.txt','and i am a student');
+
+// const data= fs.readFileSync('info.txt');
+// console.log(data);
+
+// fs.renameSync("info.txt","myinfo.txt");
+
+// fs.unlinkSync("myinfo.txt");
